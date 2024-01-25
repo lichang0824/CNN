35ed3304ef3c88103e2b089866873560dd738d3b
lichang0824 <78362137+lichang0824@users.noreply.github.com>
2024-01-25 14:42:24 -0800

    separate out modules, file management

---
+++ BinvoxDataset.py
@@ -0,0 +1,65 @@
+import os
+import Binvox
+import pandas as pd
+import math
+from torch.utils.data import Dataset
+class CustomDataset(Dataset):
+    def __init__(self, input_folder_path, label_file_path, transform = None, max_count = None, ram_limit = 1000):
+        self.input_folder_path = input_folder_path
+        self.label_file_path = label_file_path
+        self.transform = transform
+        self.max_count = max_count if max_count else math.inf
+        self.ram_limit = ram_limit
+        self.input_paths, self.input_names = self.load_input_paths_names()
+        self.labels = self.load_labels()
+    
+    def load_input_paths_names(self):
+        count = 0
+        list_of_file_paths = []
+        list_of_file_names = []
+        directory = os.fsencode(self.input_folder_path)
+        for file in os.listdir(directory):
+            list_of_file_paths.append(self.input_folder_path + os.fsdecode(file))
+            list_of_file_names.append(os.fsdecode(file))
+            count += 1
+            if count >= self.max_count:
+                break
+        return list_of_file_paths, list_of_file_names
+    
+    def load_labels(self):
+        csv = pd.read_csv(self.label_file_path, header = None)
+        return pd.Series(data = csv[1].values, index = csv[0].values, dtype = 'float32')
+    
+    def __len__(self):
+        return len(self.input_paths)
+    
+    def __getitem__(self, idx):
+        if idx % self.ram_limit == 0:
+            # clear existing samples in ram
+            self.samples_in_ram = None
+            # need to load new samples into ram
+            self.samples_in_ram = self.load_sample_into_ram(idx)
+        sample = self.load_sample_from_ram(self.input_names[idx])
+        
+        # debug
+        # print(self.input_names[idx].replace('binvox', 'stl'))
+        
+        if self.transform:
+            sample = self.transform(sample)
+        if idx % self.ram_limit == 0:
+            print('Processing sample number', idx)
+        return sample, self.labels[self.input_names[idx].replace('binvox', 'stl')]
+    
+    def load_sample_from_disk(self, file_path):
+        return Binvox.read_as_3d_array(open(file_path, 'rb')).data
+
+    def load_sample_into_ram(self, idx):
+        samples_in_ram = {}
+        # need to load ram_limit # of samples, starting from idx
+        print('Loading samples', idx, 'through', min(idx + self.ram_limit, self.__len__()) - 1)
+        for file_name in self.input_names[idx : min(idx + self.ram_limit, self.__len__())]:
+            samples_in_ram[file_name] = self.load_sample_from_disk(self.input_folder_path + file_name)
+        return samples_in_ram
+
+    def load_sample_from_ram(self, file_name):
+        return self.samples_in_ram[file_name]
