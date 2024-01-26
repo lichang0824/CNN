59adfba3641915667a38a2dd088733f9e5eb5b1d
lichang0824 <78362137+lichang0824@users.noreply.github.com>
2024-01-25 18:59:13 -0800

    Trying multiprocessing

--- BinvoxDataset.py
+++ BinvoxDataset.py
@@ -3,6 +3,7 @@ import Binvox
 import pandas as pd
 import math
 from torch.utils.data import Dataset
+import multiprocessing
 class CustomDataset(Dataset):
     def __init__(self, input_folder_path, label_file_path, transform = None, max_count = None, ram_limit = 1000):
         self.input_folder_path = input_folder_path
@@ -50,15 +51,30 @@ class CustomDataset(Dataset):
             print('Processing sample number', idx)
         return sample, self.labels[self.input_names[idx].replace('binvox', 'stl')]
     
-    def load_sample_from_disk(self, file_path):
-        return Binvox.read_as_3d_array(open(file_path, 'rb')).data
+    def load_sample_from_disk(self, file_name):
+        return (file_name, Binvox.read_as_3d_array(open(self.input_folder_path + file_name, 'rb')).data)
 
     def load_sample_into_ram(self, idx):
+        num_cores = multiprocessing.cpu_count()
         samples_in_ram = {}
         # need to load ram_limit # of samples, starting from idx
         print('Loading samples', idx, 'through', min(idx + self.ram_limit, self.__len__()) - 1)
+        sample_names_to_load = self.input_names[idx : min(idx + self.ram_limit, self.__len__())]
+
+        pool = multiprocessing.Pool(processes = num_cores)
+        tuples = pool.map(self.load_sample_from_disk, sample_names_to_load)
+        '''
+        results = [pool.apply_async(self.load_sample_from_disk, args = (sample_name,)) for sample_name in sample_names_to_load]
+        tuples = [result.get() for result in results]
+        '''
+        pool.close()
+        pool.join()
+        '''
         for file_name in self.input_names[idx : min(idx + self.ram_limit, self.__len__())]:
-            samples_in_ram[file_name] = self.load_sample_from_disk(self.input_folder_path + file_name)
+            samples_in_ram[file_name] = self.load_sample_from_disk(file_name)
+        '''
+        for sample in tuples:
+            samples_in_ram[sample[0]] = sample[1]
         return samples_in_ram
 
     def load_sample_from_ram(self, file_name):
