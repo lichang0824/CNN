731347b75bf2be936840dbb0dcd3b190b81d858f
lichang0824 <78362137+lichang0824@users.noreply.github.com>
2024-03-03 22:44:17 -0800

    multicore for savio

--- U-Net/Savio_Dataset.py
+++ U-Net/Savio_Dataset.py
@@ -1,32 +1,16 @@
 import os
-import Binvox
 import pandas as pd
 from torch.utils.data import Dataset
+import multiprocess as multiprocessing
 import json
+from worker import load_sample_from_savio
 class CustomDataset(Dataset):
-    def __init__(self, data_path, label_file_path, transform = None):
+    def __init__(self, data_path, label_file_path, transform = None, ram_limit = 1000):
         self.data_path = data_path
         self.label_file_path = label_file_path
         self.transform = transform
+        self.ram_limit = ram_limit
         self.labels = self.load_labels()
-        # self.input_paths, self.input_names = self.load_input_paths_names()
-
-    """
-    def load_input_paths_names(self):
-        count = 0
-        list_of_file_paths = []
-        list_of_file_names = []
-        directory = os.fsencode(self.input_folder_path)
-        for file in os.listdir(directory):
-            if (self.label_type == 'json') and ('data/parts_0, files 1 through 3950/rotated_files/' + os.fsdecode(file).replace('binvox', 'stl') not in self.labels.index):
-                continue
-            list_of_file_paths.append(self.input_folder_path + os.fsdecode(file))
-            list_of_file_names.append(os.fsdecode(file))
-            count += 1
-            if count >= self.max_count:
-                break
-        return list_of_file_paths, list_of_file_names
-    """
     
     def load_labels(self):
         j = json.load(open(os.path.join(self.data_path, self.label_file_path)))
@@ -34,54 +18,34 @@ class CustomDataset(Dataset):
     
     def __len__(self):
         return self.labels.size
-    
-    def __getitem__(self, idx):
-        binvox_name = self.labels.index[idx].replace('rotated_files', 'Binvox_files_default_res')[:-4] + '.binvox'
-        sample_path = os.path.join(self.data_path, binvox_name)
-        sample = Binvox.read_as_3d_array(open(sample_path, 'rb')).data
-        if self.transform:
-            sample = self.transform(sample)
-        if idx % 1000 == 0:
-            print('Processing sample number', idx)
-        return sample, self.labels.iloc[idx]
-        # debug
-        # print(self.input_names[idx].replace('binvox', 'stl'))
-        """
-        if self.transform:
-            sample = self.transform(sample)
-        if idx % self.ram_limit == 0:
-            print('Processing sample number', idx)
-        if self.label_type == 'csv':
-            return sample, self.labels[self.input_names[idx].replace('binvox', 'stl')]
-        if self.label_type == 'json':
-            return sample, self.labels['data/parts_0, files 1 through 3950/rotated_files/' + self.input_names[idx].replace('binvox', 'stl')]
-    
-    def load_sample_from_disk(self, file_name):
-        return (file_name, Binvox.read_as_3d_array(open(self.input_folder_path + file_name, 'rb')).data)
 
     def load_sample_into_ram(self, idx):
         num_cores = multiprocessing.cpu_count()
         samples_in_ram = {}
         # need to load ram_limit # of samples, starting from idx
-        print('Loading samples', idx, 'through', min(idx + self.ram_limit, self.__len__()) - 1)
-        sample_names_to_load = self.input_names[idx : min(idx + self.ram_limit, self.__len__())]
-
+        print('Loading samples', idx)
+        labels_to_load = self.labels[idx : min(idx + self.ram_limit, self.__len__())].index.to_list()
+        paths_to_load = [os.join(self.data_path, label) for label in labels_to_load]
         pool = multiprocessing.Pool(processes = num_cores)
-        tuples = pool.map(self.load_sample_from_disk, sample_names_to_load)
-        '''
-        results = [pool.apply_async(self.load_sample_from_disk, args = (sample_name,)) for sample_name in sample_names_to_load]
-        tuples = [result.get() for result in results]
-        '''
+        tuples = pool.map(load_sample_from_savio, paths_to_load)
         pool.close()
         pool.join()
-        '''
-        for file_name in self.input_names[idx : min(idx + self.ram_limit, self.__len__())]:
-            samples_in_ram[file_name] = self.load_sample_from_disk(file_name)
-        '''
         for sample in tuples:
             samples_in_ram[sample[0]] = sample[1]
         return samples_in_ram
 
-    def load_sample_from_ram(self, file_name):
-        return self.samples_in_ram[file_name]
-"""
+    def load_sample_from_ram(self, file_path):
+        return self.samples_in_ram[file_path]
+    
+    def __getitem__(self, idx):
+        if idx % self.ram_limit == 0:
+            # clear existing samples in ram
+            self.samples_in_ram = None
+            # need to load new samples into ram
+            self.samples_in_ram = self.load_sample_into_ram(idx)
+        sample = self.load_sample_from_ram(os.join(self.data_path, self.labels.index[idx]))
+        if self.transform:
+            sample = self.transform(sample)
+        if idx % 1000 == 0:
+            print('Processing sample number', idx)
+        return sample, self.labels.iloc[idx]
--- U-Net/worker.py
+++ U-Net/worker.py
@@ -1,3 +1,9 @@
 import Binvox
+import os
 def load_sample_from_disk(file_path):
     return (file_path, Binvox.read_as_3d_array(open(file_path, 'rb')).data)
+
+def load_sample_from_savio(file_path):
+    binvox_name = file_path.replace('rotated_files', 'Binvox_files_default_res')[:-4] + '.binvox'
+    sample = Binvox.read_as_3d_array(open(binvox_name, 'rb')).data
+    return (file_path, sample)
